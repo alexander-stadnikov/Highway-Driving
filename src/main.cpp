b17cce47ba715de3e37fbeab07e9c3aec22bb11e
Alexander <alexander.stadnikov@gmail.com>
2021-03-05 15:16:36 +0100

    Refactoring: simplify main function

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@
 #include "helpers.h"
 #include "json.hpp"
 
-using nlohmann::json;
+void processMessage(uWS::WebSocket<uWS::SERVER>, char *, size_t, uWS::OpCode, const Map &map);
 
 int main()
 {
@@ -19,68 +19,8 @@ int main()
 
     h.onMessage([&map](uWS::WebSocket<uWS::SERVER> ws, char *data,
                        size_t length, uWS::OpCode opCode) {
-        // "42" at the start of the message means there's a websocket message event.
-        // The 4 signifies a websocket message
-        // The 2 signifies a websocket event
-        if (length && length > 2 && data[0] == '4' && data[1] == '2')
-        {
-            auto s = hasData(data);
-
-            if (s != "")
-            {
-                auto j = json::parse(s);
-
-                std::string event = j[0].get<std::string>();
-
-                if (event == "telemetry")
-                {
-                    // j[1] is the data JSON object
-
-                    // Main car's localization Data
-                    double car_x = j[1]["x"];
-                    double car_y = j[1]["y"];
-                    double car_s = j[1]["s"];
-                    double car_d = j[1]["d"];
-                    double car_yaw = j[1]["yaw"];
-                    double car_speed = j[1]["speed"];
-
-                    // Previous path data given to the Planner
-                    auto previous_path_x = j[1]["previous_path_x"];
-                    auto previous_path_y = j[1]["previous_path_y"];
-                    // Previous path's end s and d values
-                    double end_path_s = j[1]["end_path_s"];
-                    double end_path_d = j[1]["end_path_d"];
-
-                    // Sensor Fusion Data, a list of all other cars on the same side
-                    //   of the road.
-                    auto sensor_fusion = j[1]["sensor_fusion"];
-
-                    json msgJson;
-
-                    std::vector<double> next_x_vals;
-                    std::vector<double> next_y_vals;
-
-                    /**
-           * TODO: define a path made up of (x,y) points that the car will visit
-           *   sequentially every .02 seconds
-           */
-
-                    msgJson["next_x"] = next_x_vals;
-                    msgJson["next_y"] = next_y_vals;
-
-                    auto msg = "42[\"control\"," + msgJson.dump() + "]";
-
-                    ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
-                } // end "telemetry" if
-            }
-            else
-            {
-                // Manual driving
-                std::string msg = "42[\"manual\",{}]";
-                ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
-            }
-        } // end websocket if
-    });   // end h.onMessage
+        processMessage(ws, data, length, opCode, map);
+    });
 
     h.onConnection([&h](uWS::WebSocket<uWS::SERVER> ws, uWS::HttpRequest req) {
         std::cout << "Connected!!!" << std::endl;
@@ -104,4 +44,65 @@ int main()
     }
 
     h.run();
+}
+
+void processMessage(uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode, const Map &)
+{
+    // "42" at the start of the message means there's a websocket message event.
+    // The 4 signifies a websocket message
+    // The 2 signifies a websocket event
+    if (length && length > 2 && data[0] == '4' && data[1] == '2')
+    {
+        auto s = hasData(data);
+
+        if (!s.empty())
+        {
+            auto j = nlohmann::json::parse(s);
+            std::string event = j[0].get<std::string>();
+
+            if (event == "telemetry")
+            {
+                // Main car's localization Data
+                double car_x = j[1]["x"];
+                double car_y = j[1]["y"];
+                double car_s = j[1]["s"];
+                double car_d = j[1]["d"];
+                double car_yaw = j[1]["yaw"];
+                double car_speed = j[1]["speed"];
+
+                // Previous path data given to the Planner
+                auto previous_path_x = j[1]["previous_path_x"];
+                auto previous_path_y = j[1]["previous_path_y"];
+                // Previous path's end s and d values
+                double end_path_s = j[1]["end_path_s"];
+                double end_path_d = j[1]["end_path_d"];
+
+                // Sensor Fusion Data, a list of all other cars on the same side
+                //   of the road.
+                auto sensor_fusion = j[1]["sensor_fusion"];
+
+                nlohmann::json msgJson;
+
+                std::vector<double> next_x_vals;
+                std::vector<double> next_y_vals;
+
+                /**
+                    * TODO: define a path made up of (x,y) points that the car will visit
+                    *   sequentially every .02 seconds
+                    */
+
+                msgJson["next_x"] = next_x_vals;
+                msgJson["next_y"] = next_y_vals;
+
+                auto msg = "42[\"control\"," + msgJson.dump() + "]";
+
+                ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
+            }
+        }
+        else // Manual driving
+        {
+            std::string msg = "42[\"manual\",{}]";
+            ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
+        }
+    }
 }
